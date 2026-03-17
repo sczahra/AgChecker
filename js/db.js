ee5dd21413fb2513a741874667fa340ce32ba6e3
sczahra <sczahra@gmail.com>
2026-03-17 11:28:42 -0400

    Delete js directory

--- js/db.js
+++
@@ -1,9 +0,0 @@
-export async function openDB(){
-  const db = new Dexie('alpha_gal_db');
-  db.version(1).stores({
-    products: 'barcode, name, brand, last_updated',
-    metadata: 'key',
-    overrides: 'barcode'
-  });
-  return db;
-}
--- js/version.js
+++
@@ -1,3 +0,0 @@
-export const APP_VERSION = '1.2.0';
-export const RELEASE_DATE = '2026-03-12';
-export const GITHUB_URL = 'https://github.com/sczahra/AgChecker';
